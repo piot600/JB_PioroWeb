daf8950e9c597177cd0426e5ffd491649eb49955
piot600 <s22277@pjwstk.edu.pl>
2024-02-28 09:12:24 +0100

    Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./pages/general/home/home.component";
+import {CollectionComponent} from "./pages/general/collection/collection.component";
+import {AboutComponent} from "./pages/general/about/about.component";
+import {ContactComponent} from "./pages/general/contact/contact.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component:HomeComponent },
+  { path:'collection', component:CollectionComponent },
+  { path:'about', component:AboutComponent },
+  { path:'contact', component:ContactComponent }
+];
+
